00973aa0197ac532664fd889b747456227664788
Florin Pop <popflorin1705@yahoo.com>
2017-02-26 22:20:49 +0200

    moving circles

--- js/circle.js
+++ js/circle.js
@@ -8,8 +8,17 @@ class Circle{
     show() {
         noFill();
         stroke(255);
-        strokeWeight(2);
+        strokeWeight(1);
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
 
-        ellipse(this.x, this.y, this.r * 2);        
+    update(angle, parent) {
+
+        // if parent exist, redraw the position by moving the circle
+        if(parent){
+            var rSum = this.r + parent.r;
+            this.x = parent.x + rSum * cos(angle);// * Math.cos(angle);
+            this.y = parent.y + rSum * sin(angle);// * Math.sin(angle);;
+        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
-let circle;
+var circles = [];
+var nrCircles = 3;
+
+var angle = -Math.PI/2;
+var speed = 0.02;
 
 function setup() {
     createCanvas(600, 600);
 
-    circle = new Circle(width / 2, height / 2, 100);
+    createCircles();
 }
 
 function draw() {
     background(51);
 
-    circle.show();
+    circles.forEach((circle, idx) => {
+        circle.update(angle, circles[idx - 1]);
+        circle.show();
+    });
+
+    angle += speed;
+}
+
+function createCircles() {
+    for(var i=0; i<nrCircles; i++){
+
+        // if there is already a circle
+        if(circles.length > 0){
+
+            // draw it to a new position depending on it's previous circle
+            var prevCircle = circles[i-1];
+            var newR = prevCircle.r / 3;
+
+            // draw it above the prev circle
+            var newX = prevCircle.x - prevCircle.r - newR;
+            var newY = prevCircle.y - prevCircle.r - newR;
+
+            // create it on same X
+            circles.push(new Circle(newX, newY, newR));
+
+            // else create the 1st circle
+        } else {
+            circles.push(new Circle(width / 2, height / 2, 100));
+        }
+    }
 }
